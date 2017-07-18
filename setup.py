da4c4ad9dffb015b9cfe0519b081cc355f06a149
Ben Reynwar <ben@reynwar.net>
2017-07-18 13:59:07 -0700

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name = "axilent",
+    packages=['axilent',
+              ],
+    use_scm_version = {
+        "relative_to": __file__,
+        "write_to": "axilent/version.py",
+    },
+    author = "Ben Reynwar",
+    author_email = "ben@reynwar.net",
+    description = (""),
+    license = "MIT",
+    keywords = ["VHDL", "hdl", "rtl", "FPGA", "ASIC", "Xilinx", "Altera"],
+    url = "https://github.com/benreynwar/axilent",
+    install_requires=[
+    ],
+    dependency_links=[
+    ],
+)
