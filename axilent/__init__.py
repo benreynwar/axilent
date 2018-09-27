8b75025254ba3c685b0dd08805a07bad6b65bbf5
Ben Reynwar <ben@reynwar.net>
2018-09-27 08:09:50 -0700

    Tidy up setup

--- axilent/__init__.py
+++ axilent/__init__.py
@@ -1,4 +1,7 @@
 import os
 
+from axilent.version import __version__
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 coresdir = os.path.join(basedir, 'cores')
+
--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
 from setuptools import setup
 
 setup(
-    name = "axilent",
-    packages=['axilent', 'axilent.examples',
-              ],
+    name='axilent',
+    packages=['axilent', 'axilent.examples'],
     package_data={'': ['cores/*.core', 'vhdl/*.vhd']},
-    use_scm_version = {
-        "relative_to": __file__,
-        "write_to": "axilent/version.py",
+    use_scm_version={
+        'relative_to': __file__,
+        'write_to': 'axilent/version.py',
     },
-    author = "Ben Reynwar",
-    author_email = "ben@reynwar.net",
-    description = (""),
-    license = "MIT",
-    keywords = ["VHDL", "hdl", "rtl", "FPGA", "ASIC", "Xilinx", "Altera"],
-    url = "https://github.com/benreynwar/axilent",
+    author='Ben Reynwar',
+    author_email='ben@reynwar.net',
+    description=(''),
+    license='MIT',
+    keywords=['VHDL', 'hdl', 'rtl', 'FPGA', 'ASIC', 'Xilinx', 'Altera'],
+    url='https://github.com/benreynwar/axilent',
     install_requires=[
     ],
     dependency_links=[
