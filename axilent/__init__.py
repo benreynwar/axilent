ea548fe48a7af94f36ac9129e202b5986a50807d
Ben Reynwar <ben@reynwar.net>
2018-09-28 07:38:08 -0700

    Fix version import.

--- axilent/__init__.py
+++ axilent/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from axilent.version import __version__
+from axilent.version import version as __version__
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 coresdir = os.path.join(basedir, 'cores')
