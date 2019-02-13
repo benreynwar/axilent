21e722c6c8cd97c784846d216ccc73549f0ac1ad
Ben Reynwar <ben@reynwar.net>
2019-02-12 20:03:42 -0700

    Upgrade core files to CAPI2 and move into same folder as vhdl.

--- axilent/__init__.py
+++ axilent/__init__.py
@@ -3,5 +3,5 @@ import os
 from axilent.version import version as __version__
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-coresdir = os.path.join(basedir, 'cores')
+coresdir = os.path.join(basedir, 'vhdl')
 
